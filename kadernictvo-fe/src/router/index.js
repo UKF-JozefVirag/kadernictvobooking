f05b0e83c471f17cd6d805aaaab0f70b36cbccb7
JV <jozef.virag10@gmail.com>
2024-06-02 18:23:02 +0200

    added error 404 page + routing

--- kadernictvo-fe/src/router/index.js
+++ kadernictvo-fe/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
+import PageNotFoundView from "@/views/PageNotFoundView.vue";
 
 const routes = [
     {
@@ -12,6 +13,10 @@ const routes = [
         path: '/login',
         name: 'login',
         component: LoginView
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        component: PageNotFoundView
     }
 ];
 
