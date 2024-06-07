3fa84aee4381d2b17706c6d9ef8e7657f514cfd5
JV <jozef.virag10@gmail.com>
2024-06-07 14:55:56 +0200

    added functionality for patching personal data added snack component (toast)

---
+++ kadernictvo-fe/src/components/common/SnackComponent.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="text-center">
+        <v-snackbar
+            v-model="snackbar"
+            :timeout="timeout"
+            :color="color"
+            close-on-content-click
+        >
+            {{ text }}
+
+            <template v-slot:actions>
+                <v-btn
+                    :color="color"
+                    variant="text"
+                    @click="snackbar = false"
+                >
+                    Close
+                </v-btn>
+            </template>
+        </v-snackbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SnackComponent",
+    props: {
+        text: {
+            type: String,
+            required: true
+        },
+        color: {
+            type: String,
+            required: true
+        },
+        snackOpen: {
+            type: Boolean,
+            required: true
+        }
+    },
+    data() {
+        return {
+            snackbar: false,
+            timeout: 2000
+        };
+    },
+    watch: {
+        snackOpen(val) {
+            this.snackbar = val;
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
